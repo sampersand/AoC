0d3f4050d9a49592b66cc0eec07380ea578a9140
Sam Westerman <westerhack@gmail.com>
2020-12-02 18:20:25 +0000

    Update day2.rb

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,5 +1,6 @@
-lines = open('day2.txt').each_line
-  .map { _1 =~ /^(\d+)-(\d+) (.): / and [$', $3, $1.to_i, $2.to_i] }
+lines = open('day2.txt')
+  .each_line
+  .map { |line| line =~ /^(\d+)-(\d+) (.): / and [$', $3, $1.to_i, $2.to_i] }
   .compact
 
 part1 = lines.count { |str, chr, min, max| (min..max).include? str.count(chr) }
