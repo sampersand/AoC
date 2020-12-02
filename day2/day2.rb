e8b296e471f59fa13b7c443b79a1518992dd8f1b
Sam Westerman <westerhack@gmail.com>
2020-12-02 08:23:04 +0000

    Update day2.rb

--- day2/day2.rb
+++ day2/day2.rb
@@ -2,7 +2,7 @@ lines = open('day2.txt').each_line
   .map { _1 =~ /^(\d+)-(\d+) (.): / and [$', $3, $1.to_i, $2.to_i] }
   .compact
 
-part1 = lines.count { (_1.._2).include? _4.count(_3) }
+part1 = lines.count { (_3.._4).include? _1.count(_2) }
 part2 = lines.count { (_1[_3 - 1] == _2) ^ (_1[_4 - 1] == _2) }
 
 puts "Part1: #{part1}"
