781b48b0b42cc7bd229f0a0ffc558acbcace24af
Sam Westerman <westerhack@gmail.com>
2020-12-02 08:23:52 +0000

    Update day2.rb

--- day2/day2.rb
+++ day2/day2.rb
@@ -2,8 +2,8 @@ lines = open('day2.txt').each_line
   .map { _1 =~ /^(\d+)-(\d+) (.): / and [$', $3, $1.to_i, $2.to_i] }
   .compact
 
-part1 = lines.count { (_3.._4).include? _1.count(_2) }
-part2 = lines.count { (_1[_3 - 1] == _2) ^ (_1[_4 - 1] == _2) }
+part1 = lines.count { |str, chr, min, max| (min..max).include? str.count(chr) }
+part2 = lines.count { |str, chr, min, max| (str[min - 1] == chr) ^ (str[max - 1] == chr) }
 
 puts "Part1: #{part1}"
 puts "Part1: #{part2}"
