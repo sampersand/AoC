5b4c8c47c955a863fa1c89ea52e71b3e53787a12
Sam Westerman <sam.westerman@usaepay.com>
2020-12-04 14:48:30 -0800

    updated the regex

--- day4/day4.pl
+++ day4/day4.pl
@@ -16,13 +16,13 @@ for (<>) {
 	next unless 7 == keys %_;
 
 	++$part1;
-	++$part2 if /byr (?:@{[1920..2002]})/ &&
-	            /iyr (?:@{[2010..2020]})/ &&
-	            /eyr (?:@{[2020..2030]})/ &&
-	            /ecl (?:amb|blu|brn|gry|grn|hzl|oth)/ &&
+	++$part2 if /byr (@{[1920..2002]})/ &&
+	            /iyr (@{[2010..2020]})/ &&
+	            /eyr (@{[2020..2030]})/ &&
+	            /ecl (amb|blu|brn|gry|grn|hzl|oth)/ &&
 	            /pid \d{9}/ &&
 	            /hcl #[\da-f]/i &&
-	            /hgt (?:(?:@{[150..193]})cm|(?:@{[59..76]})in)/;
+	            /hgt ((@{[150..193]})cm|(@{[59..76]})in)/;
 }
 
 print <<EOS
