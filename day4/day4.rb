9ff2a21c032be6ca4cd931874ae54cc5f026158e
Sam Westerman <westerhack@gmail.com>
2020-12-04 06:04:47 +0000

    Update day4.rb

--- day4/day4.rb
+++ day4/day4.rb
@@ -32,13 +32,6 @@ open('day4.txt')
 	.map { _1.reject(&:empty?) }
 	.map { _1.join(' ').split(/[: ]/).each_slice(2).to_h }
 	.each { _1.delete 'cid' }
-	.tap { |stream|
-		stream.map(&:keys)
-			.map(&:sort)
-			.map { %w(byr ecl eyr hcl hgt iyr pid) - _1 }
-			.count(&:empty?)
-			.tap { puts "Part 1: #{_1}" }
-	}
 	.select { (1920..2002).include? _1['byr'].to_i }
 	.select { (2010..2020).include? _1['iyr'].to_i }
 	.select { (2020..2030).include? _1['eyr'].to_i }
@@ -47,4 +40,4 @@ open('day4.txt')
 	.select { %w(amb blu brn gry grn hzl oth).include? _1['ecl'] }
 	.select { _1['pid'] =~ /^\d{9}$/ }
 	.count
-	.tap { puts "Part 2: #{_1}" }
+	.display
