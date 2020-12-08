8e61121606fe555e7462a1ff08b0516de4218955
Sam Westerman <sam.westerman@usaepay.com>
2020-12-07 23:57:30 -0800

    ok, even worse now lol

--- day8/day8.pl
+++ day8/day8.pl
@@ -1,26 +1,24 @@
 # Day 8 in perl.
 # We build up a program and then `eval` it. GOTOs considered good.
 # To swap between nop and jmp, we change the '#' to 'g'.
-open A,"day8.txt";
-y/nj\n/#g /,s/.*/_$.:\$L{$.}++&&next;$_);\n/,s/acc/\$a+=(/||s/;(.)../;$1oto'_'.($.+/,push@l,$_ while<A>;
-for(0..$#l){%L=$a=0,$l[$_]=~y/#g/g#/,eval"@l",last}
-continue{$l[$_]=~y/#g/g#/}print"Part2: $a"
+open A,'day8.txt';
+y/nj\n/#g /,s/.*/_$.:\$L{$.}++&&(y|#g|g#|,next);$_);\n/,s/acc/\$a+=(/||s/;(.)../;$1oto'_'.($.+/,push@l,$_ while<A>;
+%L=$a=0,y/#g/g#/,eval"@l",last for@l;print"Part2: $a"
 
 
 ;
 =begin
-This one was extremely fun lol. Since Perl has `goto` ('cause, yknow, why wouldn't it?), I decided to build up the program and then evaluate it. Here's an example of what the built up code looks like that we evaluate: 
+Since Perl has `goto` ('cause, yknow, why wouldn't it?), I decided to build up the program and then evaluate it. Here's an example of what the built up code looks like that we evaluate: 
 ```pl
-_1:$L{$l=1}++&&next;$a+= +15 ;
-_2:$L{$l=2}++&&next;goto _463;
-_3:$L{$l=3}++&&next;$a+= +6 ;
-_4:$L{$l=4}++&&next;#oto _449;
-_5:$L{$l=5}++&&next;goto _329;
-_6:$L{$l=6}++&&next;goto _259;
-_7:$L{$l=7}++&&next;$a+= -4 ;
-_8:$L{$l=8}++&&next;$a+= +22 ;
-_9:$L{$l=9}++&&next;$a+= +11 ;
-_10:$L{$l=10}++&&next;goto _481;
+_1:$L{1}++&&next;#oto'_'.(1+ +0 );
+_2:$L{2}++&&next;$a+=( +1 );
+_3:$L{3}++&&next;goto'_'.(3+ +4 );
+_4:$L{4}++&&next;$a+=( +3 );
+_5:$L{5}++&&next;goto'_'.(5+ -3 );
+_6:$L{6}++&&next;$a+=( -99 );
+_7:$L{7}++&&next;$a+=( +1 );
+_8:$L{8}++&&next;goto'_'.(8+ -4 );
+_9:$L{9}++&&next;$a+=( +6 );
 ```
 To swap between `nop` and `jmp`, we change the `#` to `g` and toggle the comment.
 =cut
