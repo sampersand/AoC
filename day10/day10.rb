13edbea146480f4721c13e9b7f317cc654df7e89
Sam Westerman <sam.westerman@usaepay.com>
2020-12-09 22:10:10 -0800

    day10. meh

---
+++ day10/day10.rb
@@ -0,0 +1,25 @@
+LINES = open('day10.txt')
+  .reject(&:empty?)              # delete trailing line
+  .map(&:to_i)                   # convert each line to a number
+	.sort                          # sort them
+	.unshift(0)                    # add 0 to the start
+	.tap { _1.push _1.last + 3 }   # add the builtin adaptor to the end.
+
+LINES
+	.each_cons(2)                  # ever group
+	.map { _1.reduce(&:-) }        # get the diff
+	.tally                         # tally up
+	.filter_map { _1 != -2 && _2 } # ignore `_2`s
+	.reduce(&:*)                   # get the product
+	.tap { puts "Part 1: #{_1}"}   # print it out
+
+def part2(curr=0, lines=LINES, memo = { lines.max => 1 })
+	memo[curr] ||=
+		lines
+			.take_while { _1 - curr <= 3 }
+			.each_with_index
+			.map { part2 _1, lines[_2+1..], memo }
+			.sum
+end
+
+puts "Part 2: #{part2 / 2}"
--- day9/day9.pl
+++ day9/day9.pl
@@ -1,26 +1,21 @@
 # Day 9 in perl...
+# @sampersand
 
-open A,"day9.txt";while(<A>){chomp,(push@_,$_),27<(push@a,$_)and shift@a,0+{eval join$"=',',glob"{@a}+{@a},1"}->{$N=$_
-}||last}print "Part 1: $N\n";do{$N==($a+=$_[$_])and@_=sort@_[0..$_]and(print"Part 2: ",$_[0]+pop@_,"\n")&&exit for 0..
-$#_-1}while($a=shift@_);
-__END__
-35
-20
-15
-25
-47
-40
-62
-55
-65
-95
-102
-117
-150
-182
-127
-219
-299
-277
-309
-576
+        "";open A,##
+    'day9.txt' ;while(<A>){
+  (push@_,         $_),chomp,
+ (push                @a,$_)<=
+26 or                  shift@a,
+{eval                  join$" =
+',' ,                  glob"{".
+ "@a}"                ."+{@a}".
+  ",1"}->{         $N=$_}==1 ||
+    last}print"Part 1: $N\n";do
+        {$N==($a+=$_   [$_])and
+                        @_=sort
+                      @_[0..$_]
+ and(0+               print"Par"
+  ."t 2: ",         $_[0]+pop
+     @_,"\n")&&exit for 0..
+        $#_-1}while($a
+          =shift@_);
