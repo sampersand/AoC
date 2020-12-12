d91d5add76abdee29f34d9a8d94b213741896746
Sam Westerman <sam.westerman@usaepay.com>
2020-12-12 08:10:30 -0800

    cleaned it up

--- day12/day12.rb
+++ day12/day12.rb
@@ -14,15 +14,14 @@ LINES
 	}.first.rectangular.map(&:abs).sum.tap { puts "Part 1: #{_1}" }
 
 LINES
-	.reduce([0, 10+1i]){ |(ship, waypoint), (cmd,num)|
-		case cmd
-		when 'N' then waypoint += 1i*num
-		when 'S' then waypoint -= 1i*num
-		when 'E' then waypoint += num
-		when 'W' then waypoint -= num
-		when 'L' then waypoint *= 1i**(num/90)
-		when 'R' then waypoint *= 1i**(-num/90)
-		when 'F' then ship += num * waypoint
+	.each_with_object([10+1i,0]){ |(c,n), o|
+		case c
+		when 'N' then o[0] += 1i*n
+		when 'S' then o[0] -= 1i*n
+		when 'E' then o[0] += n
+		when 'W' then o[0] -= n
+		when 'L' then o[0] *= 1i**(n/90)
+		when 'R' then o[0] *= 1i**(-n/90)
+		when 'F' then o[1] += n * o[0]
 		end
-		[ship, waypoint]
-	}.first.rectangular.map(&:abs).sum.tap { puts "Part 2: #{_1}"} 
+	}.last.rectangular.map(&:abs).sum.tap { puts "Part 2: #{_1}"} 
