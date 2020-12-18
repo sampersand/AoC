00e098a0b4c1e3a0eb73e8fcdd419801835835a1
Sam Westerman <sam.westerman@usaepay.com>
2020-12-17 22:33:40 -0800

    simplified

--- day18/day18.rb
+++ day18/day18.rb
@@ -3,7 +3,7 @@ class Integer
 	alias - *
 end
 
-lines = open('day18.txt').each_line.map(&:chomp).tr('*','-')
+lines = open('day18.txt').read.tr('*','-').lines
 
 puts "Part 1: #{lines.map { eval _1 }.sum}"
 puts "Part 2: #{lines.map { eval _1.tr('+','/') }.sum}"
