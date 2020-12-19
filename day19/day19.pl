2516b936ebf67c24bcab4b62e7d07fc9b8b07647
Sam Westerman <sam.westerman@usaepay.com>
2020-12-18 22:51:55 -0800

    Day2

--- day19/day19.pl
+++ day19/day19.pl
@@ -1,24 +1,19 @@
-open STDIN, 'day19.txt';
-# $stdin = DATA
-# $stdin = open('day19.txt')
-$/="\n\n";
-$_=<>;
-s/\d+:/$&/g;
-print;
-__END__
+$stdin = open('day19.txt')
+
 rules=gets("\n\n")
 	.sub('8: 42','\& 8 | 42')
 	.sub('11: 42 31', '\& | 42 11 31')
-	.gsub(/\d+/,'\g<x\&>')
-	.gsub(/\\g<x(\d+)>:(.*)$/, '(?<x\1>\2)')
+	.gsub(/\b\d+\s/,'\g<x\&>')
+	.gsub(/\d+:(.*)$/, '(?<x\1>\2)')
 	.gsub(/\s/,"")
+	# .display
+	# exit
 	.gsub('"','')
 	.gsub(/\A/,'(?=')
 	.gsub(/\z/,')?\A\g<x0>\Z')
 	.then { Regexp.new _1 }
 
-ARGF.each_line.count { rules =~ _1 }
-	.tap(&:display)
+ARGF.count { rules =~ _1 }.tap(&:display)
 	# .split("\n")
 	# .map {
 
