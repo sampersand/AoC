6e207705ba76c2df6e975caf47113624afae34df
Sam Westerman <sam.westerman@usaepay.com>
2020-12-23 17:17:11 -0800

    added 20.rb

--- day20/day20.rb
+++ day20/day20.rb
@@ -1,178 +1,119 @@
+require 'set'
+
+
+def n(b); b.first end
+def s(b); b.last end
+def e(b); b.map(&:last) end
+def w(b); b.map(&:first) end
+
+class Tile
+	attr_reader :name, :board
+
+	def initialize(name, board)
+		@name = name
+		@board = board
+	end
+
+	def init_perm!
+		@permutation = permutations.first
+	end
+
+	def inspect; "Tile(#@name, #{!!defined? @permutation})" end
+
+	def permutations;
+		@permutations ||=
+			[@board, @board.transpose]
+				.map { [_1, _1.reverse, _1.map(&:reverse), _1.reverse.map(&:reverse)] }
+				.flatten(1)
+				.to_set
+				.freeze
+	end
+
+	def inner
+		@permutation[1..-2].map { _1[1..-2] }.freeze
+	end
+
+	def permute(edge, tile_is_of_self)
+		permutation = (permutations.find { edge == send(tile_is_of_self, _1) } or return).freeze
+		@permutation = permutation
+	end
+
+	def neighbors
+		@neighbors ||= begin
+			t = $tiles.dup
+			t.delete self
+			[t.find { _1.permute(n(@permutation), :s) }.tap { t.delete _1 },
+			 t.find { _1.permute(s(@permutation), :n) }.tap { t.delete _1 },
+			 t.find { _1.permute(e(@permutation), :w) }.tap { t.delete _1 },
+			 t.find { _1.permute(w(@permutation), :e) }]
+		end
+	end
+
+	def to_s
+		@permutation.map(&:join).join("\n")
+	end
+end
+
 $/=""
 
-SQUARES = Hash.new { |h, k| h[k] = [] }
-TILES={}
+$tiles=Set.new
 
 open("day20.txt").map(&:chomp).each do |l|
 	name, *lines = l.split("\n")
-	name = name[/\d+/].to_i
-	lines = lines.map(&:chars)
-
-	[lines, lines.transpose]
-		.map { [_1, _1.reverse, _1.map(&:reverse), _1.reverse.map(&:reverse)] }
-		.flatten(1)
-		.each do |tile|
-			(TILES[name]||=[]).push tile
-			[tile.first, tile.last, tile.map(&:first), tile.map(&:last) ].each do |edge|
-				SQUARES[edge.join].push([name, TILES[name].length])
-			end
-		end
+	$tiles.add Tile.new(name[/\d+/].to_i, lines.map(&:chars))
 end
 
-NEIGHBORS = {}
-TILES.each do |name, perms|
-	neighbors = SQUARES.select { _2.map(&:first).uniq.include?(name) }
-		.transform_values { |v| v.reject { |n, _| n == name} }
-		.values.map { _1.map(&:first) }.flatten.uniq
-	NEIGHBORS[name] = neighbors
+$/="\n"
+
+def make_grid(tile, neighbors, x=0, y=0)
+	neighbors[[x, y]] || !tile and return
+	neighbors[[x, y]] = tile
+	nbrs = tile.neighbors
+
+	make_grid nbrs[0], neighbors, x, y-1
+	make_grid nbrs[1], neighbors, x, y+1
+	make_grid nbrs[2], neighbors, x+1, y
+	make_grid nbrs[3], neighbors, x-1, y
 end
 
-def shared_neighbor(n1, n2)
-	NEIGHBORS[n1].find { NEIGHBORS[n2].include? _1 and _1 != n1 and _1 != n2 }
-end
+make_grid $tiles.first.tap(&:init_perm!), neighbors = {}
+min = neighbors.keys.min
 
-class Array; def pr; map(&:join).join("\n") end end
-
-corner, (n1, n2) = NEIGHBORS.select { _2.length == 2 }.first
-
-board = [[corner, n1], [n2]]
-board[-1].push shared_neighbor(n1, n2)
-
-loop do 
-	x = NEIGHBORS.keys
-			.product(NEIGHBORS.keys)
-			.find { |l, r| shared_neighbor(board[-2][-1], l) == r && shared_neighbor(board[-1][-1], r) == l } \
-			or break
-	board[0].push x[0]
-	board[1].push x[1]
-	p board
-end
-
-
-p x
-# 10.times do
-# 	board
-# end
-
-def concat_rows(*row)
-	acc = TILES[row.shift]
-	until row.empty?
-		acc = acc.product(TILES[row.shift])
-			.find { |l, r| l.map { _1[-1] } == r.map { _1[0] } }
-			.reduce { |l, r| l.zip(r).map { _1 + _2 } }#{ _1[..-2] + _2[1..] } }
+# right about here's where i said "screw it, i just want to get the answer."
+ary = (min[1]..min[1]+11).map do |y|
+	(min[0]..min[0]+11).map do |x|
+		neighbors[[x, y]]
 	end
-	acc
 end
 
-# puts concat_rows(corner, n1, x.first).map(&:join)
-# puts concat_rows(n2, board[-1][-1]).map(&:join)
-# 	row.each_cons
-# # p TILES[x1].map{ _1.pr }
-# puts TILES[x1].map { _1.map(&:join) }.product(TILES[x2].map { _1.map(&:join)}).to_a
-# 	.find { |l,r| l.map { _1[-1] }  == r.map { _1[0] } }
-# 	.reduce { |l, r| l.zip(r).map { _1[..-2] + _2[1..] }}
-	# .map { |l, r| p l, r } # x, y = a.map { _1.map(&:join) }; p x, y ; exit.reduce(&:zip).join("\n") }# .join#.reduce(&:zip).map { |l, r| _1.join + _2.join + "\n" } }.join
-	# .map { |x,y| p [x, y] }
+grid =
+	ary
+		.map { |l| l.reduce(Array.new(8){""}) { |a, r|
+				a.zip(r.inner.map(&:join)).map { _1 + _2 }
+			}.join("\n")
+	}.join("\n")
 
+class String;
+	def monsters
+	split("\n").map { |line| line.chars.each_with_index.map { eval("\"\\u01%02x\""%_2) + _1 }.join }.join("\n")
+	.gsub!(%r{\
+(?<=$|\n)(?<a>.*)(?<b>[^#.@])(?<c>....................................)#(?<d>.*\n)\
+^(?<e>.*)\k<b>#(?<f>.........)#(?<g>.)#(?<h>.........)#(?<i>.)#(?<j>.........)#(?<k>.)#(?<l>.)#(?<m>.*\n)\
+^(?<n>.*)\k<b>(?<o>..)#(?<p>.....)#(?<q>.....)#(?<r>.....)#(?<s>.....)#(?<t>.....)#}, <<'EOS'.gsub(/\s/,"")
+\k<a>\k<b>\k<c>.\k<d>
+\k<e>\k<b>.\k<f>.\k<g>.\k<h>.\k<i>.\k<j>.\k<k>.\k<l>.\k<m>
+\k<n>\k<b>\k<o>.\k<p>.\k<q>.\k<r>.\k<s>.\k<t>.
+EOS
+)&.gsub(/[^.\n#]/,'')&.then { _1.monsters || _1 }
+end end
 
-
-# board.map! { }
-# board.flatten!.map! { TILES[_1] }
-# puts board.map { _1.(&:join).join("\n") }
-# p board
-# def assemble(tiles, so_far={})
-# 	if tiles.empty?
-# 		yield so_far
-# 		return
-# 	end
-
-# 	(name, _), *tiles = tiles
-# 	edges = SQUARES.select { _2.map(&:first).uniq.include? name }.values
-# 	edges.each do |community|
-# 		next if community.any? { |name, _| so_far[name] }
-# 		permutations, neighbors = community.partition { _1[0] == name }
-# 		permutations.map(&:last).each do |perm|
-# 			assemble tiles, so_far.merge(name => perm) do |res|
-# 				yield res
-# 			end
-# 		end
-# 	end
-
-# 	$names||=begin require 'set'; Set.new end
-# 	$names.add? name and p $names
-# end
-
-# assemble TILES.to_a do |x|
-# 	p x
-# 	exit
-# end
-
-__END__
-board = [[corner, n1], [n2, NEIGHBORS[n1].find { NEIGHBORS[_1].-([corner]).include? n2 }]]
-
-# xxx
-# xxx
-100.times do
-	p NEIGHBORS[board[-2].last].find { |x| NEIGHBORS[board[-1].last].any? { _1 == x }}
-	p NEIGHBORS[board[-1].last]
-	# n1 = p(NEIGHBORS[n1].select { NEIGHBORS[_1].include? n2 }.-([corner]))[0]
-	exit
-	# n1
-	# exit
-end
-p row
-
-# p neighbors
-
-exit
-__END__
-def assemble(tiles, so_far=[nil])
-	(name, perms), *tiles = tiles
-	return if so_far.include?(name)
-	edges = SQUARES.filter_map { (x=_2.map(&:first).uniq).include?(name) && [_1, x - [name]] }
-	p edges
-end
-
-p assemble TILES.to_a
-
-
-# start = 
-# 	TILES.filter_map { |name, _|
-# 		edges = SQUARES.filter_map { x=_2.map(&:first).uniq; x.include?(name) && [_1, x - [name]] } .to_h
-# 		edges.map { _2.length }.count(0) == 4 or next
-# 		p edges
-# 		# p edges.select { _2.length == 2 }
-# 		exit
-# 		name
-# }
-
-# p edges
-# .flatten.sort.count(0)
-exit
-
-fst, snd = tiles.to_a.first(2)
-# p SQUARES
-p SQUARES.select { |_, l| l.map(&:first).include?(fst) && l.map(&:first).include?(snd) }.length
-exit
-(l, li), (r, ri) = SQUARES.values.first.uniq(&:first)
-puts tiles[l][li-1].map(&:join)
-puts
-puts tiles[r][ri-1].map(&:join)
-p SQUARES.values.select { _1.empty? }.length
-
-	# .map { x= _1.split("\n"); [x.shift[/\d+/], x.to_a.map { |l| l.chars.map {|x| x == '#'} }.then { |q| [*q] }]}
-	# .to_h
-
-# p lines.keys.shuffle.first(4).map(&:to_i).reduce(&:*)
-__END__
-things =
-	lines.lazy
-		.map { |l,r|[r, r.reverse, r.map(&:reverse), r.reverse.map(&:reverse)].map { [l, _1] } }
-
-		# .map 
-		.to_a.display
-
-# 	.to_a.display
-# # p lines.length ** 4
-# # pp lines.first
-# # pp lines
+print "Part 2: "
+(grid.monsters || 
+	grid.split("\n").reverse.join("\n").monsters ||
+	grid.split("\n").map { _1.chars.reverse.join }.join("\n").monsters ||
+	grid.split("\n").map { _1.chars.reverse.join }.reverse.join("\n").monsters ||
+	grid.split("\n").map(&:chars).transpose.map(&:join).join("\n").monsters ||
+	grid.split("\n").reverse.join("\n").monsters ||
+	grid.split("\n").map { _1.chars.reverse.join }.join("\n").monsters ||
+	grid.split("\n").map { _1.chars.reverse.join }.reverse.join("\n").monsters ||
+	grid).tap { puts _1,_1.count('#') }
