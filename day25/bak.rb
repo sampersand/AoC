edf2afb6acd3f40cc71058e3c0e992b8af9d8f36
Sam Westerman <sam.westerman@usaepay.com>
2020-12-24 22:16:14 -0800

    cleaned up

--- day25/bak.rb
+++ day25/bak.rb
@@ -1,60 +1,11 @@
-# pk1 = 5764801
-# ls1 = 8
-# pk2 = 17807724
-# ls2 = 11
-
-pk1 = 15733400
-pk2 = 6408062
-
-ls1 = 3903333
-ls2 = 10459425
+cardpub, doorpub = open('day25.txt').map(&:to_i)
 
 v = 1
-subj = pk1
-ls2.times do 
-		v *= subj
-		v %= 20201227
-end
-p v
-v = 1
-subj = pk2
-ls1.times do 
-		v *= subj
-		v %= 20201227
-end
-p v
-=begin calculator
-v = 1
-subj = 1
 (1..).each do |i|
-	v = (v * subj) % 20201227
+	v = (v * 7) % 20201227
 
-	if v == pk1
-		puts "pk1: #{i}"
-		pk1 = nil
-		exit unless pk2
-	elsif v == pk2
-		puts "pk2: #{i}"
-		pk2 = nil
-		exit unless pk1
+	if (c = v == cardpub) || v == doorpub
+		puts (c ? doorpub : cardpub).pow i, 20201227
+		exit
 	end
-	# p i if i % 10_000 == 0
 end
-=end calculator
-
-
-__END__
-cpk = 5764801
-sn = 7
-v = 1
-ls = 8
-ls.times do 
-	v *= sn
-end
-
-v = sn ** ls
-
-p v  % 20201227
-
-require 'prime'
-p 20201226.prime?
--- day25/day25.rb
+++ day25/day25.rb
@@ -1,11 +1,12 @@
 cardpub, doorpub = open('day25.txt').map(&:to_i)
 
-v = 1
-(1..).each do |i|
-	v = (v * 7) % 20201227
 
-	if (c = v == cardpub) || v == doorpub
-		puts (c ? doorpub : cardpub).pow i, 20201227
-		exit
-	end
-end
+(1..)
+	.lazy
+	.map { [_1, 7.pow(_1, 20201227)] }
+	.find { _2 == cardpub || _2 == doorpub }
+	.then { (_2 == cardpub ? doorpub : cardpub).pow _1, 20201227 }
+	.display
+
+
+	
