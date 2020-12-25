d13eff19562b22d080fbe4c528e31421c38d7f00
Sam Westerman <sam.westerman@usaepay.com>
2020-12-24 22:25:28 -0800

    removed whitespace

--- day25/day25.rb
+++ day25/day25.rb
@@ -1,12 +1,8 @@
 cardpub, doorpub = open('day25.txt').map(&:to_i)
 
-
 (1..)
 	.lazy
 	.map { [_1, 7.pow(_1, 20201227)] }
 	.find { _2 == cardpub || _2 == doorpub }
 	.then { (_2 == cardpub ? doorpub : cardpub).pow _1, 20201227 }
 	.display
-
-
-	
