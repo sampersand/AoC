d8b393bbcbc0ac6138edf465af40545c98d6f95e
Sam Westerman <sam.westerman@usaepay.com>
2021-02-05 09:54:37 -0800

    Renamed to day4.pl correctky

--- 2020/day04/day4.pl
+++ 2020/day04/day4.pl
@@ -1,28 +1,16 @@
-# Day 4 in perl.
-# Awful.
-open STDIN, 'day4.txt';
+# AOC 2020, Day 4, in perl.
+# We had to help customs agents validate passports.
 
-$/='';
-$"='|';
+open STDIN,'day4.txt'; # omit if reading from stdin
 
-for (<>) {
-	s/cid:\S+//;
-
-	7 == split or next;
-	$part1++;
-
-	$part2++ if (join "", sort split) =~ /
-		byr:(@{[1920..2002]})
-		ecl:(amb|blu|brn|gr[yn]|hzl|oth)
-		eyr:20(2\d|30)
-		hcl:\#[0-9a-fA-F]{6}
-		hgt:((@{[150..193]})cm|(@{[59..76]})in)
-		iyr:20(1\d|20)
-		pid:\d{9}
-	/x;
-}
-
-print <<EOS
-Part 1: $part1
-Part 2: $part2
-EOS
+$/='';for(<>){s/cid:\S+//;7==splitonext;00;$"=
+'|'            ;$o                         ++;
+;$t   ++if&w   =~/ byr: (@{[1920..2002]})  (?#
+#@#  %      %  ##) ecl: (amb|blu|brn|gr[yn ]#(
+|hz  l |  | o  th) eyr: 20(2\d|30)         (?#
+@#@  |      |  $#) hcl: \#[0-9a-fA-F]{6}   #!2
+(?#  : x__x :  #?) hgt: ((@{[150..193]})c  (?#
+)m|  (  @@  )  in) iyr: 20(1\d|20)         (?#
+(?#   9.__.7   #?) pid: \d{9}              /x}
+sub     w(     _){                         @@=
+59..78;join"",sort split}print"P1:$o\nP2:$t\n"
