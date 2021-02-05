e5ff1a173b8a072a254c3b95413d4e23928f1807
Sam Westerman <sam.westerman@usaepay.com>
2021-02-05 09:33:54 -0800

    removed dead code

--- 2020/day02/day2.pl
+++ 2020/day02/day2.pl
@@ -17,18 +17,3 @@ open STDIN,'day2.txt'; # Omit if reading from stdin
      "\n"                             ;say
     $o  ,$                           t;  "\
    /X    X\                         'X    X"
-
-;
-=finish
-               @@=0;for(<>){/^(
-              ?# Please fix my #)
-    (\d+)(    ?#  passwords!   )-    (\d+)\ 
-    (.):/x  ;$t++if(substr($',$      1,1)eq
-    $3)^$3                        eq substr
-      $' ,$     2,1;$m=$1;$x=$2;$_=    $'
-    ;$z=s/     (?#   Uhh ok?    #)   $3//gx
-   ;+ $o        ++if grep{$_==$z}   $m .. $x
-      }$                               ,=
-     "\n"                             ;say
-    $o  ,$                           t;  "\
-   /X    X\                         'X    X"
