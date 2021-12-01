77919f44dcbd83490cb8e9f1d9a8d370f7d09cde
SamW <mail@sampersand.me>
2021-12-01 00:03:55 -0800

    added day1.hs

---
+++ 2021/day1/day1.hs
@@ -0,0 +1,23 @@
+module Main where
+import System.IO  
+import Control.Monad
+
+-- main = putStrLn "Hello World"
+
+readLines :: String -> IO [String]
+readLines file = lines <$> readFile file
+
+
+part1 :: [Int] -> Int
+part1 ns = length $ filter (\(a, b) -> a < b) $ zip ns (tail ns)
+
+part2 :: [Int] -> Int
+part2 ns = part1 $ zipWith3 (\a b c -> a + b + c) ns (tail ns) (tail $ tail ns)
+
+main :: IO ()
+main = do
+   -- alternative: contents :: [Int] = map read . lines <$> readFile "day1.txt"
+   contents1 <- readLines "day1.txt"
+   let contents = (read <$> contents1) :: [Int]
+   putStrLn $ "part1: " ++ (show $ part1 contents)
+   putStrLn $ "part2: " ++ (show $ part2 contents)
--- 2021/day1/day1.rb
+++ 2021/day1/day1.rb
@@ -2,10 +2,10 @@ lines = open('day1.txt').map(&:to_i)
 
 p lines
   .each_cons(2)
-  .count { _1 < _2 }
+  .count { |_1,_2| _1 < _2 }
 
 puts lines
   .each_cons(3)
   .map(&:sum)
   .each_cons(2)
-  .count { _1 < _2 }
+  .count { |_1,_2| _1 < _2 }
