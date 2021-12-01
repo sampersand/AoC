a5112e0a2cb96173db9833dfaab52f6e803130af
SamW <mail@sampersand.me>
2021-12-01 12:04:31 -0800

    pudated day1

--- 2021/day1/day1.rb
+++ 2021/day1/day1.rb
@@ -1,11 +1,11 @@
 lines = open('day1.txt').map(&:to_i)
 
-p lines
+puts lines
   .each_cons(2)
-  .count { |_1,_2| _1 < _2 }
+  .count { _1 < _2 }
 
 puts lines
   .each_cons(3)
   .map(&:sum)
   .each_cons(2)
-  .count { |_1,_2| _1 < _2 }
+  .count { _1 < _2 }
