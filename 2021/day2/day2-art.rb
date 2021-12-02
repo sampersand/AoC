b683074cb2a8553a1be6243ef2234e8e7dbe326c
SamW <mail@sampersand.me>
2021-12-01 21:46:15 -0800

    made it a bit better

--- 2021/day2/day2-art.rb
+++ 2021/day2/day2-art.rb
@@ -5,7 +5,7 @@
 h=       d1=d2=0;open(
 "da"  "y2.txt").each{|l|
  l=~/\s/;x=$'.to_i;if$`[0
- ]==?f;h+=x;d2+=a*x;else;
+ ]=="f";h+=x;;d2+=a*x;else
  a+=x*=$`=~/u/?-1:1;d1+=x
 end};puts"part1: #{d1*h}"
 puts  %Q(part2: #{d2*h})
