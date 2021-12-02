e69860fe79f8c76f9b30d2afe6d1dbe05133583e
SamW <mail@sampersand.me>
2021-12-01 21:41:36 -0800

    Added day2

---
+++ 2021/day2/day2.rb
@@ -0,0 +1,23 @@
+lines = open('day2.txt').map(&:chomp)
+
+horiz = depth = 0
+
+lines.each do |line|
+  case line
+  when /forward (\d+)/ then horiz += $1.to_i
+  when /down (\d+)/    then depth += $1.to_i
+  when /up (\d+)/      then depth -= $1.to_i
+  end
+end
+
+puts "part1: #{horiz * depth}"
+
+horiz = depth = aim = 0
+lines.each do |line|
+  case line
+  when /forward (\d+)/ then horiz += $1.to_i ; depth += aim * $1.to_i
+  when /down (\d+)/    then aim += $1.to_i
+  when /up (\d+)/      then aim -= $1.to_i
+  end
+end
+puts "part2: #{horiz * depth}"
