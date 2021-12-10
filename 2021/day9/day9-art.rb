d8062c2daa544dbc3d0524dde2b0fe52b03f75b2
SamW <mail@sampersand.me>
2021-12-09 21:36:05 -0500

    day9 art. not terrific

--- 2021/day9/day9-art.rb
+++ 2021/day9/day9-art.rb
@@ -1,19 +1,21 @@
-
-###       #
-####    ###
-###### #####    
-#############  ##
-#################
-################
-__END__
-
+# Day 9: we had to pilot lava tubes.
+# (This is a volcano with lava tubes...)
 $stdin = open('day9.txt')
-H={}
 
-$<.zip(0..){|l,x|l[..-2].chars.zip(0..){|c,y|c==?9||H[x+y.i]=c.to_i}}
-A=->z{[z-1,z+1,z+1i,z-1i].select &H}
-B=->x,f{A[x].reject(&f[x]=f).map{B[_1,f]};f.size}
-l=H.select{_2<A[_1].map(&H).min}.keys
+            H   =
+           {}   ;\
+          $<.   zip\
+         (0..   ){|l,
+        x|l[0   ..-2].
+       chars.   zip(0..
+){|c  ,y|c==+   ?9||H[x+ y.i]=c
+.to_i   }};A=   ->z{[z-  1,z+1,
+z-1i,1i     +   z].     select(
+&H)};B=->x,     f{f[x   ]=1;A[x
+].reject(&f).   map{  B[_1,f]};
+f.size};qa=l=   H.   select{_2<
+A[_1].map(&H)     .min}.keys;p(
+"1:#{l.sum(&H   )} 2:#{l.map{B[
+_1,{}]}.max(3   ).reduce:*}");0
 
-puts "part1: #{l.sum &H}"
-puts "part2: #{l.map{B[_1,{}]}.max(3).reduce:*}"
+
