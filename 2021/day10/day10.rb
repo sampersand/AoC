4e0aaca2ab9fbfe4c7f470ca5e0b0174d621d4dd
Sam Westerman <sam@sampersand.me>
2022-01-11 02:28:02 +0000

    Update day10.rb

--- 2021/day10/day10.rb
+++ 2021/day10/day10.rb
@@ -1,4 +1,4 @@
-$stdin=open('day10.txt')
+$stdin = open('day10.txt')
 
 part1 = 0
 part2 = []
@@ -6,13 +6,12 @@ $stdin.each_line do |line|
   line.chomp! # delete trailing newline
   nil while line.gsub! /\[\]|\(\)|\{\}|<>|\n/, '' # strip all matching pairs
 
-
   if line =~ /[\])}>]/
     # if we have a corrupt line, then add it to part1
-    part1 += { ')' => 3, ']' => 57, '}' => 1197, '>' => 25137}[$&]
+    part1 += { ')' => 3, ']' => 57, '}' => 1197, '>' => 25137 }[$&]
   else
     # otherwise, add the calculated value to part2
-    part2.push line.reverse.each_char.reduce(0) { |n, c| p [n, c]; n*5 + ' ([{<'.index(c) }
+    part2.push line.reverse.each_char.reduce(0) { _1*5 + ' ([{<'.index(_2) }
   end
 end
 
