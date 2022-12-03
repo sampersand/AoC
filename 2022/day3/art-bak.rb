c7c034e793996139b8a5d8b0822015116c069204
SamW <mail@sampersand.me>
2022-12-03 12:55:40 -0500

    added day3's art

--- 2022/day3/art-bak.rb
+++ 2022/day3/art-bak.rb
@@ -1,22 +1,37 @@
+# Day 3. I drank a coffee to stay awake for the challenge
 $stdin = open 'day3.txt'
 
-lines = $stdin.map(&:chars)
-r = ->a,b=0{[?1,*?a..?z,*?A..?Z].index a}
-t = lines.each_slice(3).sum { r[*_1.reduce(:&)] }
-o = lines.sum{r[*_1&_1.slice!(..._1.size/2)]}
-
-part1 = lines.sum { |items| 
-  first = items[...items.size/2]
-  second = items[items.size/2..]
-  common_item = (first & second)[0]
-  prioritize(common_item)
-}
-
+              (
+          r  =
+         ->  a,
+         b    =
+          0{
+[?1,*'a'..'z',*'A'..'Z']
+ .index a});t=(l=$<.map &\
+ :chars).each_slice(03)   .
+ sum{r[*_1.reduce(:&)]}   ;
+   o=l.sum{n=_1.size   ./2
+       r[*_1&_1.
+slice!(...n)]};puts(t,o)
 __END__
+$stdin = open 'day3.txt'
+
+l = $<.map(&:chars)
+r = ->a,b=0{[?1,*?a..?z,*?A..?Z].index a}
+t = l.each_slice(3).sum{r[*_1.reduce(:&)]}
+o = l.sum{r[*_1&_1.slice!(..._1.size/2)]}
 
 
-
-##################
-##################
-##################
-##################
+              #
+          #  #
+         ##  ##
+         #    #
+          #
+########################
+ #########################
+ ######################   #
+ ######################   #
+   #################   ###
+       #########
+###########################
+raise unless p([t,o]) ==[2633,7785]
--- 2022/day3/art.rb
+++ 2022/day3/art.rb
@@ -1,16 +1,16 @@
+# Day 3. I drank a coffee to stay awake for the challenge
 $stdin = open 'day3.txt'
 
-data = $stdin.map(&:chars)
-r = ->a,b=0{[?1,*?a..?z,*?A..?Z].index a}
-t = data.each_slice(3).sum { r[*_1.reduce(:&)] }
-o = data.sum{r[*_1&_1.slice!(..._1.size/2)]}
+              (
+          r  =
+         ->  a,
+         b    =
+          0{
 
-__END__
-
-########
-###########
-########  
-########
-########
-########
-########
+[?1,*'a'..'z',*'A'..'Z']
+ .index a});t=(l=$<.map &\
+ :chars).each_slice(03)   .
+ sum{r[*_1.reduce(:&)]}   ;
+   o=l.sum{n=_1.size/  02r
+       +r[*_1&_1.
+slice!(...n)]};puts(t,o)
