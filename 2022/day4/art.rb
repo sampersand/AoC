46a00c1229fe9f9ac5215f90d53d34e2c1178eea
SamW <mail@sampersand.me>
2022-12-04 11:32:38 -0500

    added the art

--- 2022/day4/art.rb
+++ 2022/day4/art.rb
@@ -1,10 +1,17 @@
+# Day 4: Elves had to clean up their sections
+
 $stdin = open 'day4.txt'
+                               o=
+                               t=
+                               0;
+                               $<
+.map                           {;
+   %r:                         ([
+     \d]  +)-(                 \d
+      +),(\d+)-(               \d
+    +):x=~_1;t+=1if           []!=
+   (x=*$1..$2)&(y=*$3      ..$4);o+=\
+    1if(q=x.&y)==x        ||q==y};p([+
+     o,t]);%W[%            / 7 /\ V \
+      `4%p;/              ( : A  | \ ]
 
-o = t = 0 ; $< . map {
-  / ( \d + ) - ( \d + ) , ( \d + ) - ( \d + ) /x =~ _1
-  t += 1 if [] != ( x = * $1 .. $2 ) & ( y = * $3 .. $4 )
-  o += 1 if ( q = x & y ) == x || q == y
-}
-p [o, t]
-
-raise unless [o,t] == [424,804]
--- 2022/day4/day4.rb
+++ 2022/day4/day4.rb
@@ -3,6 +3,7 @@ $stdin = open 'day4.txt'
 p1 = p2 = 0
 $stdin.each do |line|
   x, y = line.split(',').map { |part| eval part.sub('-', '..') }
+  raise [x,y].inspect if y.cover?(x) && !x.cover?(y)
 
   p1 += 1 if x.cover?(y) || y.cover?(x)
   p2 += 1 unless (x.to_a & y.to_a).empty?
