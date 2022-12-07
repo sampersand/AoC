fde10e73dc6feb6e9e67a519cfb40bab5b7b1275
SamW <mail@sampersand.me>
2022-12-07 13:23:39 -0500

    added day7

--- 2022/day07/art-bak.rb
+++ 2022/day07/art-bak.rb
@@ -1,23 +1,24 @@
-
-# Set standard in
 $stdin = open 'day7.txt'
 
-require'pathname'
-
 F=[]
-LINES = $stdin.read.lines
+D=->{s=0;$<.map{_1=="$ cd ..\n"&&break;s+=_1[/\$ c/]?D[]:_1.to_i};F<< s;s}
+C=D[]
+p [F.select{_1<1e5}.sum,F.select{C-4e7<=_1}.min]
 
-def doit
-  a = 0
-  LINES.each do |l|
-    a += doit if l =~ ''
-n=Pathname'';$<.map{
-  _1.chop! # todo: figure out how to remove this
-  _1=~/\$ cd /&&n/=$'
-  (F[n]||=[])<<(_1=~/dir /?F[n/$']:_1.to_i)
-}
 
-s=->v{Integer===v ?v:v.sum(&s)};
-p [o=F.select{_1<1e5}.sum,t=F.select{F[0]-4e7<=_1}.min]
+__END__
 
-fail unless o==919137 && t==2877389
+
+
+#########
+#######################
+#######################
+#######################
+#######################
+#######################
+#######################
+
+
+
+146x30 for the upper part, then it fully declines by  181
+entire thing is 400 long, 250 tall without it
--- 2022/day07/art.rb
+++ 2022/day07/art.rb
@@ -1,6 +1,10 @@
+# Day 7: We had to navigate folders on a computer
 $stdin = open 'day7.txt'
 
-F=[]
-D=->{s=0;$<.map{_1=="$ cd ..\n"&&break;s+=_1[/\$ c/]?D[]:_1.to_i};F<< s;s}
-C=D[]
-p [F.select{_1<1e5}.sum,F.select{C-4e7<=_1}.min]
+A=[];D=proc\
+{s=0;$<.map{_1['..']&&\
+break;s+=_1["$ c"]?D[]:
++_1.to_i};s.tap{A<<+_1}
+};C=D[];p([A.select{_1<
+1e5}.sum,A.select{C-("|
+\s";4e7)<=_1}.min]);/#/
