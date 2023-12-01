37f395a50e3a8b95edbec4ecefdb545704b24619
SamW <mail@sampersand.me>
2023-12-01 13:53:11 -0800

    updated gitignore

--- 2023/day1/day1.rb
+++ 2023/day1/day1.rb
@@ -1,4 +1,12 @@
-$stdin = open 'day1.txt'
+require '../submitter'
+require '../shorthand'
+using Aoc
+
+lines = download! 2023, 1, lines: true
+p lines
+exit
+$stdin = lines
+# $stdin = open 'day1.txt'
 a={'one'=>1,'two'=>2,'three'=>3,'four'=>4,'five'=>5,'six'=>6,'seven'=>7,'eight'=>8,'nine'=>9,'zero'=>0}
 k=Regexp.union(/\d/, *a.keys)
 p $stdin.sum {"#{_1[k]; a[$&]||$&}#{_1.rindex k; a[$&]||$&}".to_i }
