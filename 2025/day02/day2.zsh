0b237b1f134f01ed6f432a4ceb508cb89f1b98d0
SamW <mail@sampersand.me>
2025-12-02 16:35:18 -0800

    day2 zsh

--- 2025/day02/day2.zsh
+++ 2025/day02/day2.zsh
@@ -1,26 +1,15 @@
 #!/bin/zsh
 setopt rematchpcre
-found=0
-for raw_line in ${(s:,:):-$(cat input.txt)}; do
-	range=(${(s:-:)raw_line})
-	readonly start=$range[1]
-	readonly stop=$range[2]
 
-	echo $raw_line
-	for (( i=start; i < stop; i++  )) {
-		if [[ $i =~ '^(\d+)\1$' ]] {
-			let found+=$i
-		}
+let part1=0 part2=0
+for raw_line in ${(s:,:):-$(cat ${@:-input.txt})}; do
+	range=( ${(s:-:)raw_line} )
+
+	for (( i=range[1]; i < range[2]; i++ )) {
+		if [[ $i =~ '^(\d+)\1$' ]] let part1+=i;
+		if [[ $i =~ '^(\d+)\1+$' ]] let part2+=i;
 	}
 done
-echo $found
-# lines = File.read('input.txt')
-#   .split(',')
-#   .map { it.split('-').map(&:to_i)  }
-#   .map { _1.._2 }
 
-# puts lines.sum { |range| range.select { /^(\d+)\1$/ =~ it.to_s }.sum }
-# puts lines.sum { |range| range.select { /^(\d+)\1+$/ =~ it.to_s }.sum }
-
-# val=0202
-# expr "$val" : '\([0-9][0-9]*\)\1' >/dev/null && printf "%s\n" "$val"
+echo part1: $part1
+echo part2: $part2
