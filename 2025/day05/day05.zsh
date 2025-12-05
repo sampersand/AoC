f63bdc3521796211197e0909a849d6351a978465
SamW <mail@sampersand.me>
2025-12-04 23:14:18 -0800

    basic zsh

--- 2025/day05/day05.zsh
+++ 2025/day05/day05.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-if [[ ! -t 0 ]] exec < sample.txt
+if [[ ! -t 0 ]] exec < input.txt
 
 ranges=()
 fresh=()
@@ -22,6 +22,24 @@ foreach id ($fresh)
 	end
 end
 
+functions -M in 3; in () { (( $2 <= $1 && $1 <= $3 )); true }
+function merge {
+	local a=($=1)
+	local b=($=2)
+	if (( in(a[1], b[1], b[2]) && in(a[2], b[1], b[2]) )) { REPLY=$2; return }
+	if (( in(b[1], a[1], a[2]) && in(b[2], a[1], a[2]) )) { REPLY=$1; return }
+
+	local dir=$(( a[1] != b[1] ? a[1] < b[1] : a[2] < b[2] ))
+	local -a earlier later
+	if (( dir )) {
+		earlier=($a); later=($b)
+	} else {
+		earlier=($b); later=($a)
+	}
+	if (( earlier[2] + 1 < later[1] )) return 1
+	REPLY="$earlier[1] $later[2]"
+}
+
 p $part1
 
 fresh_ranges=()
@@ -29,33 +47,29 @@ while (( $#ranges )) do
 	range=$ranges[1]; shift ranges
 
 	overlaps=()
-	for ((fresh_ranges
+	x=()
+	foreach tmp ($fresh_ranges)
+		if merge $tmp $range; then
+			overlaps+=$REPLY
+		else
+			x+=$tmp
+		fi
+	end
+	fresh_ranges=($x)
+
+	if (( !$#overlaps )) {
+		fresh_ranges+=$range
+	} else {
+		ranges+=($overlaps)
+	}
 done
 
-function merge {
-}
-exit
-exit
-class Range
-  def merge?(other)
-    overlap?(other) and Range.new(*[*other.minmax, *minmax].minmax)
-  end
+integer part2
+foreach x y ($=fresh_ranges)
+	let "part2+=$y-$x + 1"
 end
 
-fresh_ranges = []
-while (range = ranges.pop)
-  overlaps = []
-  fresh_ranges.reject! { tmp = range.merge?(it) and overlaps << tmp }
+if (( $part1 != 885 )) echo bad part1: $part1
+if (( $part2 != 348115621205535 )) echo bad part2: $part2
 
-  if overlaps.empty?
-    # if nothing overlaps, add the range to the known part1 ranges
-    fresh_ranges << range
-  else
-    # add anything that did overlap back into the input range list
-    ranges.concat overlaps
-  end
-end
-
-part2 = fresh_ranges.sum(&:count)
-fail unless part1 == 885
-fail unless part2 == 348115621205535
+echo $part1 $part2
